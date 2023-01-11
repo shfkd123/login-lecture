d1bfbd7d1a0835667292837fd18dc57d6eb12506
shfkd123 <shfkd123@gmail.com>
2023-01-11 23:05:40 +0900

    etc

--- app/app.js
+++ app/app.js
@@ -26,12 +26,12 @@ const app = express();
 
 
 //라우팅
-const home = require("./src/routes/home");
+const home = require("../src/routes/home");
 
 //앱 세팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
-app.use(express.static(`${__dirname}/src/public`));
+app.use(express.static(`${__dirname}/src/public`)); // 정적 경로 추가 
 app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드.
 
 module.exports = app;
